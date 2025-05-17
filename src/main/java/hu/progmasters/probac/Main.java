8f3576d9bbb04690812d716dd9bdd5775ccdaa23
BukkfalviZoltan <bukkfalviz@gmail.com>
2025-05-17 17:14:03 +0200

    Formatted readme.md

--- src/main/java/hu/progmasters/probac/Main.java
+++ src/main/java/hu/progmasters/probac/Main.java
@@ -1,16 +1,32 @@
 package hu.progmasters.probac;
 
+/**
+ * Converts string into Morse Code
+ */
 public class Main {
+    /**
+     * Starts main code
+     * @param args String array standard start parameter
+     */
     public static void main(String[] args) {
         printMorseCode("Wozu Worte drucken, es gibt doch Schreiber");
     }
 
+    /**
+     * Returns index of letter in alphabet.
+     * @param letter String whose index is to be found in alphabet.
+     * @return index of letter
+     */
     static int getPosInAlphabet(String letter) {
         letter = letter.toLowerCase();
         String alphabet = "abcdefghijklmnopqrstuvwxyz1234567890";
         return alphabet.indexOf(letter);
     }
 
+    /**
+     * prints String converted into Morse code
+     * @param input String that is to be converted
+     */
     static void printMorseCode(String input) {
         input = input.toLowerCase();
         input = input.replaceAll("[^a-z0-9]", ""); // removes everything but letters and digits
